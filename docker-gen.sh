9147484ad8e0817c10ff475bdf6728a2953adb99
bwees <brandonwees@gmail.com>
2025-09-30 12:21:46 -0500

    chore: use pg16 everywhere

--- docker-gen.sh
+++ docker-gen.sh
@@ -8,7 +8,7 @@ IMMICH_VERSION=v1.138.0
 images="
 ghcr.io/immich-app/immich-server:$IMMICH_VERSION
 docker.io/valkey/valkey:8-bookworm
-ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0
+ghcr.io/immich-app/postgres:16-vectorchord0.4.3-pgvectors0.2.0
 "
 
 # Pull each image sequentially
